180604078db0196c732550ca3acdce108c6a6760
KirA-a-a <kiraaaakka@gmail.com>
2022-12-19 12:21:55 +0300

    add linter

--- scenes/taskMy.js
+++ scenes/taskMy.js
@@ -1,18 +1,22 @@
-const { Telegraf, Markup, Scenes, session, Composer} = require('telegraf');
+const {
+  Scenes, Composer,
+} = require('telegraf');
 
-const taskList = require("./taskAdd.js")
+const taskList = require('./taskAdd.js');
 
-const startStep = new Composer()
-startStep.on("text", async (ctx) => {
-    try {
-        ctx.replyWithHTML(`Твои задачи: `)
-        ctx.replyWithHTML(`${taskList.taskList}`)
-        console.log(taskList.taskList)
-        return ctx.scene.leave()
-    } catch (e) {
-        console.error(e)
+const startStep = new Composer();
+startStep.on('text', async (ctx) => {
+  try {
+    if (taskList.taskList.length === 0) {
+      ctx.replyWithHTML('<b>Ты пока не просил запомнить меня какие-то задачи</b>');
+    } else if (taskList.taskList.length !== 0) {
+      ctx.replyWithHTML(`Список твоих задач: ${taskList.taskList}.`);
     }
-})
+    return ctx.scene.leave();
+  } catch (e) {
+    console.error(e);
+  }
+});
 
-const myTasksScene = new Scenes.WizardScene("myTasksWizard", startStep)
-module.exports = myTasksScene
+const myTasksScene = new Scenes.WizardScene('myTasksWizard', startStep);
+module.exports = myTasksScene;
--- scenes/wheatherInfo.js
+++ scenes/wheatherInfo.js
@@ -1,36 +1,38 @@
-const { Telegraf, Markup, Scenes, session, Composer} = require('telegraf');
+const {
+  Scenes, Composer,
+} = require('telegraf');
 const axios = require('axios');
-require('dotenv').config()
+require('dotenv').config();
 
-const startStep = new Composer()
-startStep.on("text", async (ctx) => {
-    try {
-        ctx.wizard.state.data = {}
-        await ctx.replyWithHTML("<b>Отправь мне свою геолокацию</b>")
-        return ctx.wizard.next()
-    } catch (e) {
-        console.error(e)
+const startStep = new Composer();
+startStep.on('text', async (ctx) => {
+  try {
+    ctx.wizard.state.data = {};
+    await ctx.replyWithHTML('<b>Отправь мне свою геолокацию</b>');
+    return ctx.wizard.next();
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+const geoStep = new Composer();
+geoStep.on('message', async (ctx) => {
+  try {
+    ctx.wizard.state.data.title = ctx.message.location;
+    if (ctx.message.location) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=___&units=metric`;
+      const response = await axios.get(url);
+      ctx.reply(`${response.data.name}: ${response.data.main.temp} °C`);
+    } else {
+      ctx.reply('Что-то это не похоже на геолокацию, попробуй еще раз, пожалуйста. \n'
+                + '\np.s. Геолокацию нужно скидывать из самого телеграма.\n\n'
+                + 'нажав на скрепочку => геолокация');
     }
-})
+    return ctx.scene.leave();
+  } catch (e) {
+    console.error(e);
+  }
+});
 
-const geoStep = new Composer()
-geoStep.on("message", async (ctx) => {
-    try {
-        ctx.wizard.state.data.title = ctx.message.location
-        if (ctx.message.location) {
-            const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=___&units=metric`
-            const response = await axios.get(url);
-            ctx.reply(`${response.data.name}: ${response.data.main.temp} °C`);
-        } else {
-            ctx.reply('Что-то это не похоже на геолокацию, попробуй еще раз, пожалуйста. \n' +
-                '\np.s. Геолокацию нужно скидывать из самого телеграма.\n\n' +
-                'нажав на скрепочку => геолокация');
-        }
-        return ctx.scene.leave()
-    } catch (e) {
-        console.error(e)
-    }
-})
-
-const weatherScene = new Scenes.WizardScene("weatherWizard", startStep, geoStep)
-module.exports = weatherScene
+const weatherScene = new Scenes.WizardScene('weatherWizard', startStep, geoStep);
+module.exports = weatherScene;
